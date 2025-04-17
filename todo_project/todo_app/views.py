548d962dbd20a33ca41c63741f9e0c0777ff021b
hitesh1305 <142805472+hitesh1305@users.noreply.github.com>
2025-04-17 23:14:32 +0530

    Add files via upload

---
+++ todo_project/todo_app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+from .forms import TaskForm
+
+def task_list(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+
+    return render(request, 'todo_app/task_list.html', {'tasks': tasks, 'form': form})
+
+def complete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = True
+    task.save()
+    return redirect('task_list')
+
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('task_list')
